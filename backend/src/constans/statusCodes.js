68109e1da637d0526a5c786509323669ad478621
syaron147 <syaronkulungrai@gmail.com>
2026-08-09 08:47:04 +0545

    accesstoken and refrestoken created

---
+++ backend/src/constans/statusCodes.js
@@ -0,0 +1,18 @@
+export const STATUS_CODES={
+    //success
+    OK:200,
+    CREATED:201,
+    NO_CONTENT:204,
+
+ 
+    BAD_REQUEST:400,
+    UNAUTHORIZED:401,
+    FORBIDDEN:403,
+    NOT_FOUND:404,
+    CONFLICT:409,
+    UNPROCESSABLE_ENTITY:422,
+
+    // server errors
+    INTERNAL_SERVER_ERROR:500
+
+}
