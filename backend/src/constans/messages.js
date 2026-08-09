7b9a71f123ec564b84f5171f8fda17b8a19f6904
syaron147 <syaronkulungrai@gmail.com>
2026-08-09 08:48:27 +0545

    auth middleare completed

---
+++ backend/src/constans/messages.js
@@ -0,0 +1,18 @@
+export const MESSAGES={
+    // authentication
+    REGISTER_SUCCESS:"User regisered sucessfully",
+    LOGIN_SUCCESS:"User login successfully",
+    LOGOUT_SUCCESS:"User logout SUCCESSFULLY",
+    INVALID_CREDENTIALS:"Invalid email or password",
+    UNAUTHORIZED:"Unauthorized access",
+    FORBIDDEN:"Access denied",
+    EMAILALREADY_EXIST:"Email already exist",
+    PHONE_ALREADY_EXIST:"PHone ALready exist",
+    USER_NOT_FOUND:"User not found",
+    ACCOUNT_INACTIVE:"Account has beed inactive",
+    TOKEN_REFRESHED:"Token has been refreshed",
+    Invalid_REFRESH_TOKEN:"Invalid refresh token",
+    Invalid_ACCESSTOKEN_TOKEN:"invalid accessToken",
+    VALIDATION_FAILED:"Validation failed",
+    INTERNAL_SERVER_ERROR:"Internal server error"
+}
