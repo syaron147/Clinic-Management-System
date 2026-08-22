919ef401ec25cd9a8db17dfe878a1efcbb8bd625
syaron147 <syaronkulungrai@gmail.com>
2026-08-22 20:33:13 +0545

    implement medical records module with CRUD routes, services and validation schemas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,10 @@ import express from "express"
 import authRoutes from "../module/auth/auth.routes.js"
 import adminRoutes from "../module/auth/admin.routes.js"
 import patientRoutes from "../module/patient/patient.routes.js"
+import doctorRoutes from "../module/doctor/doctor.routes.js"
+import appointmentRoutes from "../module/appointment/appointent.routes.js"
+import departmentRoutes from "../module/department/department.routes.js"
+import medicalRecordRoutes from "../module/medicalRecord/medicalRecord.routes.js"
 
 const router = express.Router();
 
@@ -14,9 +18,9 @@ router.get("/health",(req,res)=>{
 router.use("/auth",authRoutes)
 router.use("/admin",adminRoutes)
 router.use("/patient",patientRoutes)
-
-
-
-
+router.use("/doctor",doctorRoutes)
+router.use("/appointment",appointmentRoutes)
+router.use("/department",departmentRoutes)
+router.use("/medical-record",medicalRecordRoutes)
 
 export default router
